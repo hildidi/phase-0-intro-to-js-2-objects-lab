cefda2ad50feec5ec102f5130d0be7ac5e7ada4e
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-07 14:41:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+// const employee = {
+//     name: "Sam",
+//     streetAddress: "55 Water St",
+// };
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newEmployee = {...employee}; //This creates a clone of the original object and saves it into a new variable. 
+//     newEmployee[key]=value;
+//     return newEmployee;
+// }
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key]=value;
+//     return employee;
+// }
+// function deleteFromEmployeeByKey(employee, key){
+//     const newObject = Object.assign({}, employee, "")
+//     delete newObject[key] //deleting the key/value pair.
+//     return newObject; // returning the object.
+// }
+// //Returns employee w/o the deleted key/value pair:
+// //Mofidy the original employee:
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee.name;
+//     return employee;    
+// }
+
+
 const employee = {
-    name: "Sam",
-    streetAddress: "55 Water St",
+    name: 'Sam',
+    streetAdress: '11 Broadway'
 };
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee = {...employee}; //This creates a clone of the original object and saves it into a new variable. 
-    newEmployee[key]=value;
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee = {...employee};
+    newEmployee[key] = (value);
     return newEmployee;
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key]=value;
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value; 
     return employee;
 }
-function deleteFromEmployeeByKey(employee, key){
-    const newObject = Object.assign({}, employee, "")
-    delete newObject[key] //deleting the key/value pair.
-    return newObject; // returning the object.
+
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = Object.assign({}, employee);//Object.assig (, employee) creates a copy.
+        delete newEmployee[key]; //deletes key from the new array
+        return newEmployee;
+
 }
-//Returns employee w/o the deleted key/value pair:
-//Mofidy the original employee:
-function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
     delete employee.name;
-    return employee;    
+    return employee;
 }
 
-
- 
